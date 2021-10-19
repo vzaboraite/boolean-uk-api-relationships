91783ea0e372a9e5070b4d45be1ed45cbb4be5b4
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-19 21:12:33 +0100

    Build `getUserWithOrders` controller for `users` resource

--- src/resources/users/controller.js
+++ src/resources/users/controller.js
@@ -48,4 +48,25 @@ const getUserWithAddress = async (req, res) => {
   }
 };
 
-module.exports = { getAll, getOneById, getUserWithAddress };
+const getUserWithOrders = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      include: {
+        orders: true,
+      },
+    });
+
+    res.json({ user: result });
+  } catch (error) {
+    console.error({ error: error.message });
+
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAll, getOneById, getUserWithAddress, getUserWithOrders };
--- src/resources/users/router.js
+++ src/resources/users/router.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { getAll, getOneById, getUserWithAddress } = require("./controller");
+const {
+  getAll,
+  getOneById,
+  getUserWithAddress,
+  getUserWithOrders,
+} = require("./controller");
 
 const router = express.Router();
 
 router.get("/", getAll);
 
 router.get("/:id/address", getUserWithAddress);
+
+router.get("/:id/orders", getUserWithOrders);
 
 router.get("/:id", getOneById);
 
