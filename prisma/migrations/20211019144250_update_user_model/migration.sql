056eb0e504331ff365cb9e6108580e38f029df01
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-19 15:44:26 +0100

    Update `User` model.

---
+++ prisma/migrations/20211019144250_update_user_model/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `firstName` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - You are about to alter the column `lastName` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "firstName" SET DATA TYPE VARCHAR(255),
+ALTER COLUMN "lastName" SET DATA TYPE VARCHAR(255);
