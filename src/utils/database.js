ef16978056622f71b9891ed9978cdda1003f9c08
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-19 14:55:24 +0100

    Update `database.js` to see debug logs.

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,5 +1,16 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+  ],
+});
+
+prisma.$on("query", async (event) => {
+  console.log({ event });
+});
 
 module.exports = prisma;
