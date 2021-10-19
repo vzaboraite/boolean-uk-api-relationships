580e08c8d438dc49edc77c7cf9e7edca4998f7b8
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-19 15:41:35 +0100

    Build `Order` model.

---
+++ prisma/migrations/20211019144042_create_order_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
